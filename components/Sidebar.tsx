10087317c36b5854e54e8ddf6a13aaeede703a96
Sarthak <sarthakaggarwal2310@gmail.com>
2025-02-05 20:07:31 +0530

    Sidebar V0.2

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import Box from "./Box";
+import SidebarItem from "./Sidebaritems";
 
 
 interface SidebarProps {
@@ -34,20 +35,15 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 
     return (
         <div className="flex h-full">
-            <div 
-                className="
-                hidden
-                md:flex
-                flex-col
-                gap-y-2
-                bg-black
-                h-full
-                w-[300px]
-                p-2
-            "
-            >
+            <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
                 <Box>
-                    Sidebar Navigation
+                    <div className="flex flex-col gap-y-4 px-5 py-4"> {routes.map((item) => (
+                        <SidebarItem
+                            key={item.label}
+                            {...item} />
+                    ))}
+
+                    </div>
                 </Box>
                 <Box className="overflow-y-auto h-full">
                     Song Library
