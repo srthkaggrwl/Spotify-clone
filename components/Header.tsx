1c2ce818550ae7867bf97d9b4879dcede62bc77b
Sarthak <sarthakaggarwal2310@gmail.com>
2025-02-05 22:38:29 +0530

    Header V0.1

---
+++ components/Header.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useRouter } from "next/navigation"; // Correct import for server components
+import { FiTable } from "react-icons/fi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { twMerge } from "tailwind-merge";
+
+interface HeaderProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({children, className}) => { 
+    const router = useRouter();
+
+    const handleLogout = () => {
+        // Handle logout in the future.
+    }
+    return (
+        <div className={twMerge(`
+            h-fit
+            bg-gradient-to-b
+            from-emerald-800
+            p-6
+        `,
+            className
+        )}>
+        
+        <div className="
+            w-full
+            mb-4
+            flex
+            items-center
+            justify-between
+        ">
+            <div className="
+                hidden
+                md:flex
+                gap-x-2
+                items-center
+            ">
+            <button 
+              onClick={() => router.back()}
+              className="
+                rounded-full
+                bg-black
+                flex
+                items-center
+                justify-center
+                hover:opacity-75
+                transition
+            ">
+                <RxCaretLeft className="text-white" size={35} />    
+            </button>    
+
+            <button 
+              onClick={() => router.forward()}
+              className="
+                rounded-full
+                bg-black
+                flex
+                items-center
+                justify-center
+                hover:opacity-75
+                transition
+            ">
+                <RxCaretRight className="text-white" size={35} />    
+            </button>  
+
+            </div>
+          </div>
+        </div>
+    );
+}
+
+
+export default Header;
